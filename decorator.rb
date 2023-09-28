eab874c313860b9fe2ec626882d6798960a6de1f
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-09-28 11:50:16 +0300

    Fix linter errors

--- decorator.rb
+++ decorator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'nameable'
-# Decorator class
+# Decorator class for use with capitalize and trim classes
 class Decorator < Nameable
   attr_reader :nameable
 
